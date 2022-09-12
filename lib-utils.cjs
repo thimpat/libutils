e67074d41f1e7649214eb229b745b1dd0fd81b2c
Patrice Thimothee <thimpat@gmail.com>
2022-09-12 21:16:32 +0100

    FEAT: Add function local IP network related

--- lib-utils.cjs
+++ lib-utils.cjs
@@ -6,6 +6,7 @@
 // Built-ins
 // ==================================================================
 /** to-esm-browser: remove **/
+const os = require("os");
 const path = require("path");
 const fs = require("fs");
 const https = require("https");
@@ -404,7 +405,7 @@ const normaliseRealPath = (filepath) =>
         {
             return {
                 success: false,
-                error: new Error(`The source file "${filepath}" does not exist`)
+                error  : new Error(`The source file "${filepath}" does not exist`)
             };
         }
 
@@ -413,28 +414,28 @@ const normaliseRealPath = (filepath) =>
         if (lstats.isFile())
         {
             return {
-                success: true,
+                success : true,
                 filepath: normalisePath(filepath, {isFolder: false})
             };
         }
         else if (lstats.isDirectory())
         {
             return {
-                success: true,
+                success : true,
                 filepath: normalisePath(filepath, {isFolder: true})
             };
         }
 
         return {
             success: false,
-            error: new Error(`This method only handles files and folders ${filepath}`)
+            error  : new Error(`This method only handles files and folders ${filepath}`)
         };
     }
     catch (e)
     {
         return {
             success: false,
-            error: e
+            error  : e
         };
     }
 };
@@ -1258,6 +1259,159 @@ const getPackageJson = ({projectName = "", root = false, startDir = undefined} =
     return null;
 };
 
+/**
+ * @link https://stackoverflow.com/questions/3653065/get-local-ip-address-in-node-js
+ * @returns {{}|[]|null}
+ */
+const getIps = ({allInfo = false} = {}) =>
+{
+    try
+    {
+        const {networkInterfaces} = os;
+        const nets = networkInterfaces();
+        const results = {};
+        const arr = [];
+
+        for (const name of Object.keys(nets))
+        {
+            for (const net of nets[name])
+            {
+                const familyV4Value = typeof net.family === "string" ? "IPv4" : 4;
+                if (net.family === familyV4Value && !net.internal)
+                {
+                    results[name] = results[name] || [];
+
+                    if (allInfo)
+                    {
+
+                        arr.push(Object.assign({name}, net));
+                    }
+                    else
+                    {
+                        results[name].push(net.address);
+                    }
+                }
+            }
+        }
+
+        if (allInfo)
+        {
+            return arr;
+        }
+
+        return results;
+    }
+    catch (e)
+    {
+        console.error({lid: 4321}, e.message);
+    }
+
+    return null;
+};
+
+const getIpInfoList = () =>
+{
+    try
+    {
+        const list = getIps({allInfo: true});
+
+        for (let i = 0; i < list.length; ++i)
+        {
+            let priorities = 0;
+
+            const info = list[i];
+            const networkName = info.name.toLowerCase();
+            const address = info.address;
+            const netmask = info.netmask;
+            let cidr;
+            cidr = info.cidr.split("/");
+            cidr[1] = parseInt(cidr[1]);
+
+            if (networkName.includes("vmware") || networkName.includes("vmnet") || networkName.includes("vEthernet") || networkName.includes("(WSL)") || networkName.includes("virtual"))
+            {
+                priorities += 1;
+            }
+            else if (networkName.includes("wifi") || networkName.includes("wi-fi") || networkName.includes("wireless"))
+            {
+                priorities += 3;
+            }
+            else if (/\bethernet/.test(networkName) || /\bppp/.test(networkName))
+            {
+                priorities += 5;
+            }
+
+            if (address.startsWith("172."))
+            {
+                priorities += 1;
+            }
+            else if (address.startsWith("192.168."))
+            {
+                priorities += 3;
+            }
+
+            if (netmask.includes("255.255.255."))
+            {
+                priorities += 3;
+            }
+            else if (netmask.includes("255.255."))
+            {
+                priorities += 1;
+            }
+
+            if (cidr[0].startsWith("172."))
+            {
+                priorities += 1;
+            }
+            else if (cidr[0].startsWith("192.168."))
+            {
+                priorities += 4;
+            }
+            else if (cidr[0].startsWith("192.168.1."))
+            {
+                priorities += 5;
+            }
+
+            if (cidr[1] >= 24)
+            {
+                priorities += 2;
+            }
+            else if (cidr[1] >= 16)
+            {
+                priorities += 1;
+            }
+
+            info.priority = priorities;
+        }
+
+        return list.sort((info1, info2) => info1.priority > info2.priority ? -1 : 1);
+    }
+    catch (e)
+    {
+        console.error({lid: 4321}, e.message);
+    }
+
+    return [];
+};
+
+/**
+ * Try to best guess local ip
+ * @returns {string|(() => AddressInfo)|(() => (AddressInfo | {}))|(() => (AddressInfo | string | null))|boolean}
+ */
+const getLocalIp = () =>
+{
+    try
+    {
+        const list = getIpInfoList();
+        return list[0].address;
+    }
+    catch (e)
+    {
+        console.error({lid: 4321}, e.message);
+    }
+
+    return false;
+};
+
 
 // Generic functions
 module.exports.convertArrayToObject = convertArrayToObject;
@@ -1315,4 +1469,9 @@ module.exports.isPackageInstalled = isPackageInstalled;
 module.exports.isCjs = isCjs;
 module.exports.findProjectDir = findProjectDir;
 module.exports.getPackageJson = getPackageJson;
+
+// Network related functions
+module.exports.getIps = getIps;
+module.exports.getIpInfoList = getIpInfoList;
+module.exports.getLocalIp = getLocalIp;
 
