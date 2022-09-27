3789c160f968f481d37ab63a548b7a53ad265ecb
Patrice Thimothee <thimpat@gmail.com>
2022-09-27 23:32:52 +0100

    TEST: Add tests

--- test/unit.specs.cjs
+++ test/unit.specs.cjs
@@ -1,11 +1,31 @@
 const chai = require("chai");
 const expect = chai.expect;
 
-const {areEquals, joinPath, normalisePath} = require("../lib-utils.cjs");
+const {areEquals, joinPath, normalisePath, getGlobalArguments, sleep, getLocalIp, getIps} = require("../lib-utils.cjs");
 
 describe("In the libUtils library", function ()
 {
     this.timeout(10000);
+
+    describe("The function getGlobalArguments", () =>
+    {
+        it("to throw an exception", () =>
+        {
+            expect(getGlobalArguments).to.throw("Obsolete function: Available in version 1.9.3");
+        });
+    });
+
+    describe("The function sleep", () =>
+    {
+        it("should do a 1 second pause",  async() =>
+        {
+            const d1 = new Date();
+            await sleep(300);
+            const d2 = new Date();
+            const diffTime = (d2.getTime() - d1.getTime());
+            expect(diffTime).to.be.greaterThanOrEqual(300);
+        });
+    });
 
     describe("the function joinPath", function ()
     {
@@ -28,7 +48,6 @@ describe("In the libUtils library", function ()
         });
     });
 
-
     describe("the function areEquals", () =>
     {
         // ----------------------
@@ -36,25 +55,25 @@ describe("In the libUtils library", function ()
         // ----------------------
         it("should be true when the two arguments are both null", function ()
         {
-            const result = areEquals(null,null);
+            const result = areEquals(null, null);
             expect(result).to.be.true;
         });
 
         it("should be true when the two arguments are both undefined", function ()
         {
-            const result = areEquals(null,null);
+            const result = areEquals(null, null);
             expect(result).to.be.true;
         });
 
         it("should be true when the two arguments are both false", function ()
         {
-            const result = areEquals(false,false);
+            const result = areEquals(false, false);
             expect(result).to.be.true;
         });
 
         it("should be true when the two arguments are both true", function ()
         {
-            const result = areEquals(true,true);
+            const result = areEquals(true, true);
             expect(result).to.be.true;
         });
 
@@ -81,25 +100,25 @@ describe("In the libUtils library", function ()
         // ----------------------
         it("should be true when when the two arguments are both 0", function ()
         {
-            const result = areEquals(0,0);
+            const result = areEquals(0, 0);
             expect(result).to.be.true;
         });
 
         it("should be true when when the two arguments are two same integers", function ()
         {
-            const result = areEquals(456,456);
+            const result = areEquals(456, 456);
             expect(result).to.be.true;
         });
 
         it("should be true when when the two arguments are two same integers with opposite sign", function ()
         {
-            const result = areEquals(456,-456);
+            const result = areEquals(456, -456);
             expect(result).to.be.false;
         });
 
         it("should be false when arguments are two different integers", function ()
         {
-            const result = areEquals(556,58);
+            const result = areEquals(556, 58);
             expect(result).to.be.false;
         });
 
@@ -109,13 +128,13 @@ describe("In the libUtils library", function ()
         // ----------------------
         it("should be false when arguments are the same string", function ()
         {
-            const result = areEquals("aaaaa","aaaaa");
+            const result = areEquals("aaaaa", "aaaaa");
             expect(result).to.be.true;
         });
 
         it("should be false when arguments are two different string", function ()
         {
-            const result = areEquals("aaaaa","bbbbb");
+            const result = areEquals("aaaaa", "bbbbb");
             expect(result).to.be.false;
         });
 
@@ -124,19 +143,19 @@ describe("In the libUtils library", function ()
         // ----------------------
         it("should be true when arguments are the same array", function ()
         {
-            const result = areEquals([1, 2, 3, "ewe", "dfdf"],[1, 2, 3, "ewe", "dfdf"]);
+            const result = areEquals([1, 2, 3, "ewe", "dfdf"], [1, 2, 3, "ewe", "dfdf"]);
             expect(result).to.be.true;
         });
 
         it("should be false when arguments are two arrays with different order", function ()
         {
-            const result = areEquals([1, 2, 3],[1, 3, 2]);
+            const result = areEquals([1, 2, 3], [1, 3, 2]);
             expect(result).to.be.false;
         });
 
         it("should be false when arguments are two different arrays", function ()
         {
-            const result = areEquals([1, 2, 3, "ewe", "dfdf"],[1, 2, 3, "ewe", 0]);
+            const result = areEquals([1, 2, 3, "ewe", "dfdf"], [1, 2, 3, "ewe", 0]);
             expect(result).to.be.false;
         });
 
@@ -160,30 +179,43 @@ describe("In the libUtils library", function ()
 
         it("should be true when arguments are the same function", function ()
         {
-            const f1 = () => {};
+            const f1 = () =>
+            {
+            };
             const result = areEquals(f1, f1);
             expect(result).to.be.true;
         });
 
         it("should be true when arguments are functions implemented the same way", function ()
         {
-            const f1 = () => {};
-            const f2 = () => {};
+            const f1 = () =>
+            {
+            };
+            const f2 = () =>
+            {
+            };
             const result = areEquals(f1, f2);
             expect(result).to.be.true;
         });
 
         it("should be false when arguments are two different functions", function ()
         {
-            const f1 = () => {};
-            const f2 = () => { return 2; };
+            const f1 = () =>
+            {
+            };
+            const f2 = () =>
+            {
+                return 2;
+            };
             const result = areEquals(f1, f2);
             expect(result).to.be.false;
         });
 
         it("should be false when one argument is a function and the string value of its implementation", function ()
         {
-            const f1 = () => {};
+            const f1 = () =>
+            {
+            };
             const str = "() => {}";
             const result = areEquals(f1, str);
             expect(result).to.be.false;
@@ -202,31 +234,59 @@ describe("In the libUtils library", function ()
         // ----------------------
         it("should be true when arguments are the same object", function ()
         {
-            const result = areEquals({ff: 6, ee: 5, dd: 4, cc: 3, bb: 2, aa: 1},{ff: 6, ee: 5, dd: 4, cc: 3, bb: 2, aa: 1});
+            const result = areEquals({ff: 6, ee: 5, dd: 4, cc: 3, bb: 2, aa: 1}, {
+                ff: 6,
+                ee: 5,
+                dd: 4,
+                cc: 3,
+                bb: 2,
+                aa: 1
+            });
             expect(result).to.be.true;
         });
 
         it("should be true when arguments are the same object, but with different order of keys", function ()
         {
-            const result = areEquals({ff: 6, ee: 5, dd: 4, cc: 3, bb: 2, aa: 1},{aa: 1, bb: 2, dd: 4, cc: 3, ee: 5, ff: 6});
+            const result = areEquals({ff: 6, ee: 5, dd: 4, cc: 3, bb: 2, aa: 1}, {
+                aa: 1,
+                bb: 2,
+                dd: 4,
+                cc: 3,
+                ee: 5,
+                ff: 6
+            });
             expect(result).to.be.true;
         });
 
         it("should be false when arguments are two different objects", function ()
         {
-            const result = areEquals({ff: 6, ee: 5, dd: 4, cc: 4, bb: 4, aa: 1},{ff: 6, ee: 5, dd: 4, cc: 3, bb: 2, aa: 1});
+            const result = areEquals({ff: 6, ee: 5, dd: 4, cc: 4, bb: 4, aa: 1}, {
+                ff: 6,
+                ee: 5,
+                dd: 4,
+                cc: 3,
+                bb: 2,
+                aa: 1
+            });
             expect(result).to.be.false;
         });
 
         it("should be false when arguments are two different objects with one with a different key", function ()
         {
-            const result = areEquals({ff: 6, ee: 5, dd: 4, cc: 3, bb: 2, aa: 1},{gg: 6, ee: 5, dd: 4, cc: 3, bb: 2, aa: 1});
+            const result = areEquals({ff: 6, ee: 5, dd: 4, cc: 3, bb: 2, aa: 1}, {
+                gg: 6,
+                ee: 5,
+                dd: 4,
+                cc: 3,
+                bb: 2,
+                aa: 1
+            });
             expect(result).to.be.false;
         });
 
         it("should be false when arguments are two different objects with one missing a key", function ()
         {
-            const result = areEquals({ff: 6, ee: 5, dd: 4, cc: 3, bb: 2, aa: 1},{ff: 6, ee: 5, cc: 3, bb: 2, aa: 1});
+            const result = areEquals({ff: 6, ee: 5, dd: 4, cc: 3, bb: 2, aa: 1}, {ff: 6, ee: 5, cc: 3, bb: 2, aa: 1});
             expect(result).to.be.false;
         });
 
@@ -254,8 +314,14 @@ describe("In the libUtils library", function ()
         it("should be true when arguments are the same two complex nested array", function ()
         {
             const result = areEquals(
-                [{ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}, {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}],
-                [{ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}, {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}],
+                [
+                    {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1},
+                    {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}
+                ],
+                [
+                    {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1},
+                    {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}
+                ],
             );
             expect(result).to.be.true;
         });
@@ -263,8 +329,14 @@ describe("In the libUtils library", function ()
         it("should be false when arguments are different complex nested arrays", function ()
         {
             const result = areEquals(
-                [{ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}, {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}],
-                [{ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}, {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 5, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}],
+                [
+                    {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1},
+                    {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}
+                ],
+                [
+                    {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1},
+                    {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 5, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}
+                ],
             );
             expect(result).to.be.false;
         });
@@ -272,12 +344,28 @@ describe("In the libUtils library", function ()
         it("should be true when arguments are the same two complex nested object with depth > 1", function ()
         {
             const result = areEquals(
-                {ff: 6, ee: [1, 2, 3, "ewe",
-                             [{ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}, {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"],
-                                 dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}]], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1},
-                {ff: 6, ee: [1, 2, 3, "ewe",
-                             [{ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}, {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"],
-                                 dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}]], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1},
+                {
+                    ff                                               : 6, ee: [
+                        1, 2, 3, "ewe",
+                        [
+                            {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}, {
+                            ff                                            : 6, ee                                     : [1, 2, 3, "ewe", "dfdf"],
+                            dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1
+                        }
+                        ]
+                    ], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1
+                },
+                {
+                    ff                                               : 6, ee                                        : [
+                        1, 2, 3, "ewe",
+                        [
+                            {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}, {
+                            ff                                            : 6, ee                                     : [1, 2, 3, "ewe", "dfdf"],
+                            dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1
+                        }
+                        ]
+                    ], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1
+                },
             );
             expect(result).to.be.true;
         });
@@ -285,18 +373,52 @@ describe("In the libUtils library", function ()
         it("should be false when arguments are different two complex nested object with depth > 1", function ()
         {
             const result = areEquals(
-                {ff: 6, ee: [1, 2, 3, "ewe",
-                             [{ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}, {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"],
-                                 dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}]], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1},
-                {ff: 6, ee: [1, 2, 3, "ewe",
-                             [{ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}, {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"],
-                                 dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2}]], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1},
+                {
+                    ff                                               : 6, ee                                        : [
+                        1, 2, 3, "ewe",
+                        [
+                            {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}, {
+                            ff                                            : 6, ee                                     : [1, 2, 3, "ewe", "dfdf"],
+                            dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1
+                        }
+                        ]
+                    ], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1
+                },
+                {
+                    ff                                               : 6, ee: [
+                        1, 2, 3, "ewe",
+                        [
+                            {ff: 6, ee: [1, 2, 3, "ewe", "dfdf"], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1}, {
+                            ff                                     : 6, ee                              : [1, 2, 3, "ewe", "dfdf"],
+                            dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2
+                        }
+                        ]
+                    ], dd: 4, cc: [1, 2, 3, "ewe", "dfdf"], bb: 2, aa: 1
+                },
             );
             expect(result).to.be.false;
         });
 
 
+    });
 
+
+    describe("The function getIps", () =>
+    {
+        it("should return an ip",  async() =>
+        {
+            const ips = getIps();
+            expect(Object.keys(ips).length).to.be.greaterThan(0);
+        });
+    });
+
+    describe("The function getLocalIp", () =>
+    {
+        it("should return an ip",  async() =>
+        {
+            const localIp = getLocalIp();
+            expect(localIp).to.match(/\d+\.\d+\.\d+\.\d+/);
+        });
     });
 
 
